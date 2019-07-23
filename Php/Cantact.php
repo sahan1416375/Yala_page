c25ba7afd41be685248ceb012373c32f9b949f48
sahan1416375 <sahanhasintha12345@gmail.com>
2019-07-23 22:53:04 +0530

    asakl

--- Php/Cantact.php
+++ Php/Cantact.php
@@ -1,17 +1,24 @@
 <?php
-$con=mysqli_connect('localhost','root','','yalaTable');
-if (isset($_POST['submit']) {
+$con=mysqli_connect("localhost","root","","yalatable");
+if (isset($_POST['sumbit'])) {
 	$name=$_POST['Name'];
 	$email=$_POST['Email'];
 	$countryarr=$_POST['country'];
+	$a=implode(",", $countryarr);
 	$xxx=$_POST['xxx'];
+	$b=implode(",", $xxx);
 
-	$insert_table="INSERT INTO user (Name, Email, Country, Xxx) VALUES ('$name','$email','$countryarr','$xxx')";
+
+	$insert_table="INSERT INTO user(Name, Email, Country, Xxx) VALUES ('$name','$email','$a','$b')";
 	$insert_query=mysqli_query($con, $insert_table);
 
 	if ($insert_query) {
 		echo "Inserted!!!!!";
 	}
+	else{
+		echo "wrong!!!";
+	}
+
 }
 
 ?>
